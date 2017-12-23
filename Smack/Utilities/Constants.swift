d051b8f182f4291e9f09b1ab4c29b63c7ccc0f90
Bliss <blisswho@yahoo.com>
2017-12-23 07:37:54 -0500

    Lesson 4.

---
+++ Smack/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Bliss Hu on 12/23/17.
+//  Copyright © 2017 Bliss Hu. All rights reserved.
+//
+
+import Foundation
+
+//Segues
+let TO_LOGIN = "toLogin"
