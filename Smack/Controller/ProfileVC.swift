cc7ab15182b97484be34c5f0ec495117fc0e1810
Bliss <blisswho@yahoo.com>
2018-01-01 18:04:57 -0500

    Lesson 17

--- Smack/Controller/ProfileVC.swift
+++ Smack/Controller/ProfileVC.swift
@@ -19,24 +19,37 @@ class ProfileVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Show users image
-        profileImg.image = UIImage(named: UserDataService.instance.avatarName)
-        
-        
-        // Show users name
-        // Show users email
+        setupView()
     }
     
 
     @IBAction func closeModalPressed(_ sender: Any) {
         //Close the modal
+        self.dismiss(animated: true, completion: nil)
     }
     
     @IBAction func logoutPressed(_ sender: Any) {
-        
+        UserDataService.instance.logoutUser()
+        NotificationCenter.default.post(name: NOTIF_USER_DATA_DID_CHANGE, object: nil)
+        dismiss(animated: true, completion: nil)
     }
     
+    func setupView() {
+        // Show users image
+        
+        print("NAME: "+UserDataService.instance.name)
+        profileImg.image = UIImage(named: UserDataService.instance.avatarName)
+        profileImg.backgroundColor = UserDataService.instance.returnUIColor(components: UserDataService.instance.avatarColor)
+        userName.text = UserDataService.instance.name
+        userEmail.text = UserDataService.instance.email
+        
+        let closeTouch = UITapGestureRecognizer(target: self, action: #selector(ProfileVC.closeTap(_:)))
+        bgView.addGestureRecognizer(closeTouch)
+    }
+    
+    @objc func closeTap(_ recognizer: UITapGestureRecognizer) {
+        dismiss(animated: true, completion: nil)
+    }
     
     /*
     // MARK: - Navigation
