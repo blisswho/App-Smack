ba2f53a8a416a2f9ea8d8e08411e8c6160a9ac2a
Bliss <blisswho@yahoo.com>
2018-01-02 22:38:31 -0500

    Lesson 21

---
+++ Smack/Services/SocketService.swift
@@ -0,0 +1,13 @@
+//
+//  SocketService.swift
+//  Smack
+//
+//  Created by Bliss Hu on 1/2/18.
+//  Copyright © 2018 Bliss Hu. All rights reserved.
+//
+
+import UIKit
+
+class SocketService: NSObject {
+
+}
