76cc5701b4cd5e3eaebe6b06f034a1e30a69d7d2
Bliss <blisswho@yahoo.com>
2018-01-03 19:46:40 -0500

    Lesson 29

--- Smack/Services/MessageService.swift
+++ Smack/Services/MessageService.swift
@@ -16,6 +16,7 @@ class MessageService {
     
     var channels = [Channel]() //Array of channels
     var messages = [Message]()
+    var unreadChannels = [String]()
     var selectedChannel : Channel?
     
     func findAllChannel(completion: @escaping CompletionHandler) {
